e2480561a7c2635c64ff0284d99d7ada0da5667a
JonathanTamm <jonathan.immanuel41@ui.ac.id>
2025-09-10 16:05:42 +0700

    tes

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jonathan-immanuel41-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "jonathan-immanuel41-footballnews.pbp.cs.ui.ac.id",
+    "https://jonathan-immanuel41-footballnews.pbp.cs.ui.ac.id",
 ]
 
 
