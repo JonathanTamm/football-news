8961c43f3b9f8cd5aef1e5b00665e96158a60b14
JonathanTamm <jonathan.immanuel41@ui.ac.id>
2025-09-10 15:58:14 +0700

    test

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jonathan-immanuel41-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/jonathan.immanuel41/footballnews"
+]
+
 
 # Application definition
 
@@ -60,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
